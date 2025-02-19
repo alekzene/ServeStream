f3a0fc2d00374d65c788fa7690865f6a847fc213
— <alekzene.cs@gmail.com>
2025-02-19 17:32:49 +0800

    tailwind: set colors in theme

--- Frontend/tailwind.config.js
+++ Frontend/tailwind.config.js
@@ -2,7 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        lightPurple: "#CBA3FF",  // lighter end of gradient
+        darkPurple: "#700DCC",  // darker end of gradient
+        white: "#FFFFFF",
+        black: "#000000",
+      },
+    },
   },
   variants: {
     extend: {},
