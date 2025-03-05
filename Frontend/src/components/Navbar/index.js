71ac42c11f84912a346cd095fac7b8b28e3d8aa6
hcdevhomein <hcdevhomein@hotmail.com>
2025-03-05 10:13:46 -0500

    added the toggle button

--- Frontend/src/components/Navbar/index.js
+++ Frontend/src/components/Navbar/index.js
@@ -2,6 +2,10 @@ import { Link } from 'react-router-dom';
 import "./index.css";
 
 function Navbar() {
+  const toggleMode = () => {
+    document.body.classList.toggle('dark');
+  };
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
@@ -14,6 +18,12 @@ function Navbar() {
           <Link to="/about" className="nav-link">About Us</Link>
           <Link to="/portfolio" className="nav-link">Contact Us</Link>
           <Link to="/contact" className="button-primary btn-gradient">Book a Call</Link>
+          <button className='mode-change' onClick={toggleMode}>
+            <div className=''>
+              <img src='images/mode-light.png' className='light-icon' alt='' />
+              <img src='images/mode-dark.png' className='dark-icon hidden' alt='' />
+            </div>
+          </button>
         </div>
       </div>
     </nav>
